dac5717a6bb58051ff5b0cc73c421e770d389e7f
Måns Magnusson <mons.magnusson@gmail.com>
2014-09-01 16:09:40 +0200

    Added tests for pin_coordn_correct

---
+++ inst/tests/test-pin_coordn_correct.R
@@ -0,0 +1,11 @@
+
+cat("pin_coordn_correct : ")
+
+test_that(desc="control number",{
+  expect_equal(pin_coordn_correct(pin = "198112189876"), expected = "198112189876")
+  expect_equal(pin_coordn_correct(pin = "198112789876"), expected = "198112189876")
+  expect_equal(pin_coordn_correct(pin = "198112680000"), expected = "198112080000")
+  expect_equal(pin_coordn_correct(pin = "198112880000"), expected = "198112280000")
+})
+
+cat("\n")
