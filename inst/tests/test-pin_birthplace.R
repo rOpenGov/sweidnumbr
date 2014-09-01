68848fd3970dfa4811c6a503d3ba8ab1ff852065
Måns Magnusson <mons.magnusson@gmail.com>
2014-09-01 15:11:53 +0200

    Added test for birthplace

---
+++ inst/tests/test-pin_birthplace.R
@@ -0,0 +1,17 @@
+
+cat("pin_birthplace : ")
+
+today_pin <- paste(paste(unlist(strsplit(as.character(Sys.Date()),split = "-")), collapse = ""),"0000",sep="")
+pin_test <- c("198111219876", "198111819876","196408233234", "196408833234", today_pin, "196408830000")
+pin_test_res <-
+  c(rep("Extra number and immigrants (immigrated after 1946)", 2),
+    rep("Gotlands län", 2),
+    "Born after 31 december 1989",
+    "Stockholm stad")
+
+test_that(desc="birthplace",{
+  expect_equal(pin_birthplace(pin = pin_test), expected = as.factor(pin_test_res))
+})
+
+levels(pin_birthplace(pin = pin_test))
+cat("\n")
