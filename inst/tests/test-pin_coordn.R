1c309b6c4a2425dac9831dd7c57defefe004641f
Måns Magnusson <mons.magnusson@gmail.com>
2014-09-01 14:29:56 +0200

    Added coordn test

---
+++ inst/tests/test-pin_coordn.R
@@ -0,0 +1,11 @@
+
+cat("pin_coordn : ")
+
+pin_test <- c("198112189876", "196408233234", "198112789876", "196408833234")
+pin_test_res <- c(FALSE, FALSE, TRUE, TRUE)
+
+test_that(desc="control number",{
+  expect_equal(pin_coordn(pin = pin_test), expected = pin_test_res)
+})
+
+cat("\n")
