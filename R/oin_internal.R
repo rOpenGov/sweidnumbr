17c35ede444599834de179ba23a72f08f95922bd
Måns Magnusson <mons.magnusson@gmail.com>
2014-09-03 20:42:35 +0200

    CRAN check corrections

--- R/oin_internal.R
+++ R/oin_internal.R
@@ -15,13 +15,13 @@
 
 oin_group_element <- function(one_oin){
   switch(substr(one_oin, 1, 1),
-         "1" = "Dödsbo",
-         "2" = "Stat, landsting, kommuner, församlingar",
-         "3" = "Utländska företag som bedriver näringsverksamhet eller äger fastigheter i Sverige",
+         "1" = "D\u00F6dsbo",
+         "2" = "Stat, landsting, kommuner, f\u00F6rsamlingar",
+         "3" = "Utl\u00E4ndska f\u00F6retag som bedriver n\u00E4ringsverksamhet eller \u00E4ger fastigheter i Sverige",
          "5" = "Aktiebolag",
          "6" = "Enkelt bolag",
-         "7" = "Ekonomiska föreningar",
-         "8" = "Ideella föreningar och stiftelser",
+         "7" = "Ekonomiska f\u00F6reningar",
+         "8" = "Ideella f\u00F6reningar och stiftelser",
          "9" = "Handelsbolag, kommanditbolag och enkla bolag",
          as.character(NA))
 }
