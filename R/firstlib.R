9befb9d845e5b8b05f24c0e856943fab19ed7ff1
Måns Magnusson <mons.magnusson@gmail.com>
2014-10-13 18:46:22 +0200

    Update LICENSE and Startup message

--- R/firstlib.R
+++ R/firstlib.R
@@ -5,6 +5,6 @@
   # Sys.setlocale(locale = "UTF-8")
   # Sys.setlocale(locale = "WINDOWS-1252")
 
-  packageStartupMessage("sweidnumbr: R tools to handle swedish identity numbers.\nCopyright (C) 2014 Mans Magnusson\nhttps://github.com/MansMeg/sweidnumbr\n\n")
+  packageStartupMessage("sweidnumbr: R tools to handle swedish identity numbers.\nCopyright (C) 2014 Mans Magnusson, Erik Bulow\nhttps://github.com/MansMeg/sweidnumbr\n\n")
 
 }
