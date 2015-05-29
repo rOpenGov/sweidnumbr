8a9eead985aa4de83f35ad62d78e640c2cfd4ff0
Erik Bülow <erik.bulow@rcvast.se>
2015-05-29 12:56:51 +0200

    A blog post to introduce the package and a test data set "fake_swedes" to use for illustrative purposes.

---
+++ R/fake_swedes.R
@@ -0,0 +1,12 @@
+#' Example data
+#' 
+#' Data set with fake data to use for examples.
+#' 
+#' @format A data frame with 56 rows and 2 variables:
+#' \describe{
+#'   \item{pin}{identification number, as character}
+#'   \item{name}{Fictional Swedish names}
+#' }
+#' @name fake_swedes
+#' @docType data
+NULL
