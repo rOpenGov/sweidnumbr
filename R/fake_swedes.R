56905b9ca5f669c2b7148805ca3d88cfeb5d4376
Måns Magnusson <mons.magnusson@gmail.com>
2015-05-29 14:06:18 +0200

    Small updates in documentation of data

--- R/fake_swedes.R
+++ R/fake_swedes.R
@@ -1,10 +1,10 @@
-#' Example data
+#' Fake Swede personal identity numbers
 #' 
-#' Data set with fake data to use for examples.
+#' Data set with fake personal identity numbers and names to use as example.
 #' 
 #' @format A data frame with 56 rows and 2 variables:
 #' \describe{
-#'   \item{pin}{identification number, as character}
+#'   \item{pin}{Personal identification number, as character}
 #'   \item{name}{Fictional Swedish names}
 #' }
 #' @name fake_swedes
