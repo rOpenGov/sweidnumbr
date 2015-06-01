9f107982596f2583f7738ad1509a6663c96a292b
Erik Bülow <erik.bulow@rcvast.se>
2015-06-01 15:35:45 +0200

    Change references from MansMeg to rOpenGov. Part of #57 but does not fix it completely.

--- R/sweidnumbr.R
+++ R/sweidnumbr.R
@@ -3,7 +3,7 @@
 #' @description 
 #' Handling of swedish identity numbers. 
 #' For a quick tutorial see \code{vignette("sweidnumbr")}. 
-#' For more information see \url{https://github.com/MansMeg/sweidnumbr}.
+#' For more information see \url{https://github.com/rOpenGov/sweidnumbr}.
 #' 
 #' @name sweidnumbr
 #' @docType package
