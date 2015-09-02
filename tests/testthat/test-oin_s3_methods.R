cf9f2832005882ffb5492747efa41f0ed363bc06
Erik Bülow <erik.bulow@rcvast.se>
2015-09-02 13:35:03 +0200

    Added some test for S3 methods [ and rep

--- tests/testthat/test-oin_s3_methods.R
+++ tests/testthat/test-oin_s3_methods.R
@@ -4,6 +4,8 @@ x <- as.oin(c("556000-4615", "232100-0156", "802002-4280"))
 
 test_that("class attribut preserved", {
   expect_is(x[1], "oin")
+  expect_is(`[.oin`(x, 1), "oin")
+  expect_is(rep.oin(x, 3), "oin")
   expect_is({x[1] <- "232100-0156"; x}, "oin")
   expect_true(nchar(x[1])==11)
   expect_warning(x[1] <- "hejsan svejsan")
--- tests/testthat/test-pin_s3_methods.R
+++ tests/testthat/test-pin_s3_methods.R
@@ -4,6 +4,9 @@ x <- as.pin(c(191212121212, 198505043334))
 
 test_that("class attribut preserved", {
   expect_is(x[1], "pin")
+  expect_is(`[.pin`(x, 1), "pin")
+  expect_is(rep.pin(x, 3), "pin")
+  expect_equal(length(rep.pin(x, 3)), 6)
   expect_is({x[1] <- "8505043334"; x}, "pin")
   expect_true(nchar(x[1])==12)
   expect_warning(x[1] <- "hejsan svejsan")
